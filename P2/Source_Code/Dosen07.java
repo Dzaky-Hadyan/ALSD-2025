2d8abc99ed7afc1eb456804ffe52ca96f67879fe
Dzaky-Hadyan <dzakyhadyaneliyanta06@gmail.com>
2025-02-21 14:26:40 +0700

    feat: add latihan praktikum 2

---
+++ P2/Source_Code/Dosen07.java
@@ -0,0 +1,45 @@
+package P2.Source_Code;
+
+public class Dosen07 {
+    String idDosen, nama, bidangKeahlian;
+    boolean statusAktif;
+    int tahunBergabung;
+
+    public Dosen07() {
+
+    }
+
+    public Dosen07(String id, String nama, boolean status, int tahun, String bidang) {
+        idDosen = id;
+        this.nama = nama;
+        statusAktif = status;
+        tahunBergabung = tahun;
+        bidangKeahlian = bidang;
+    }
+
+    void tampilInformasi() {
+        System.out.println("ID: " + idDosen);
+        System.out.println("Nama: " + nama);
+        System.out.println("Status aktif: " + setStatusAktif(statusAktif));
+        System.out.println("Tahun bergabung: " + tahunBergabung);
+        System.out.println("Bidang keahlian: " + bidangKeahlian);
+    }
+
+    String setStatusAktif(boolean statusSet) {
+        if (statusSet == true) {
+            return "Aktif";
+        } else {
+            return "Tidak Aktif";
+        }
+    }
+
+    void hitungMasaKerja(int thnSkrg) {
+        int lamaBergabung = thnSkrg - tahunBergabung;
+        System.out.printf("Masa kerja: %d Tahun\n", lamaBergabung);
+    }
+
+    String ubahKeahlian(String bidang) {
+        bidangKeahlian = bidang;
+        return bidangKeahlian;
+    }
+}
---
+++ P2/Source_Code/DosenMain07.java
@@ -0,0 +1,17 @@
+package P2.Source_Code;
+
+public class DosenMain07 {
+    public static void main(String[] args) {
+        Dosen07 dsn1 = new Dosen07();
+        dsn1.idDosen = "314";
+        dsn1.nama = "Pitagoras";
+        dsn1.statusAktif = false;
+        dsn1.tahunBergabung = 2012;
+        dsn1.bidangKeahlian = "Matematika";
+        dsn1.tampilInformasi();
+        dsn1.setStatusAktif(true);
+        dsn1.hitungMasaKerja(2025);
+        dsn1.ubahKeahlian("IT");
+        dsn1.tampilInformasi();
+    }
+}
